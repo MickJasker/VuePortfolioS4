99534490c1fcba29e4879349d8755ea22742aafd
Mick Jasker <m.jasker@student.fontys.nl>
2018-03-19 12:58:12 +0100

    moved nav to separate component

--- src/App.vue
+++ src/App.vue
@@ -35,49 +35,6 @@
     text-decoration: none;
   }
 
-  nav
-  {
-    background-color: #F5F5F5;
-    border-radius:    15px;
-    height:           57px;
-    position:         fixed;
-    margin:           30px;
-    width:            calc(100% - 60px);
-    z-index:          10000;
-  }
 
-  nav ul li
-  {
-    list-style: none;
-    font-size:  18px !important;
-    display:    inline;
-    padding:    0 10px;
-  }
-
-  nav ul
-  {
-    padding:   15px;
-    min-width: 370px;
-  }
-
-  nav img
-  {
-    height:   57px;
-    padding:  5px;
-    position: static;
-    text-align: center;
-  }
-
-  #navL
-  {
-    text-align: left;
-    float:      left;
-  }
-
-  #navR
-  {
-    text-align: right;
-    float:      right;
-  }
 
 </style>
---
+++ src/components/navBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav class="elevation-10">
+    <ul id="navL">
+      <li>
+        <router-link to="/Home">Media Design</router-link>
+      </li>
+      <li>Digital Publishing</li>
+    </ul>
+    <img src="../assets/logo.svg" alt="">
+    <ul id="navR">
+      <li>SCO</li>
+      <li>UXU</li>
+      <li>DEV</li>
+      <li>PTM</li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+  export default {
+    name: "nav-bar"
+  }
+</script>
+
+<style scoped>
+
+</style>
